7ae660dabc8624300a276d0da84093f57cd2c6a0
Kiri <kirishpp@gmail.com>
2024-07-28 03:19:16 +0300

    Main script

---
+++ logsscript.lua
@@ -0,0 +1,6 @@
+game.Players.PlayerAdded:Connect(function(player)
+	print("Player ID: " .. player.UserId) -- Player ID
+	print("Player Username: " .. player.Name) -- PlayerName
+	print("Time: " .. os.date("!%Y-%m-%d %H:%M:%S", os.time() + 3*60*60))  -- GTM+3 Time
+end)
+print("New loading")
